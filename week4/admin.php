bfe4f0e8fcb7fc9b18e53c79723f2a155fccd5d9
001294495 <001294495@N202C5.labs.neit.edu>
2013-10-25 22:12:44 -0400

    The last little bit of classwork which includes working logout!!!

--- week4/admin.php
+++ week4/admin.php
@@ -9,14 +9,20 @@
         <?php
         session_start();
         session_regenerate_id();
-        
+        print_r($_GET);
         if ( empty($_SESSION["isLoggedIn"] ) ){
             header("Location:login.php");
         }
         
-        
+        if ( isset( $_GET["logout"]) && $_GET["logout"] == 1)
+        {
+            session_destroy();
+            header("Location:login.php");
+        }
         ?>
         
         <h1>You made it in!</h1>
+        <a href="admin.php?logout=1">Logout</a>
+        
     </body>
 </html>
